9a0ca6f555a1f8247e092228778ff0151b72b499
Dagny Parayao <dagny.parayao@gmail.com>
2021-07-23 19:01:32 +0000

    dagny second commit

---
+++ public/js/addMovie.js
@@ -0,0 +1,11 @@
+const submitMovie = () =>{
+    const name = document.querySelector("#name").value;
+    const description = document.querySelector("#desc").value;
+    const genre = document.querySelector("#genre").value;
+    const rating = document.querySelector("#rating").value;
+
+    console.log(name);
+    console.log(description);
+    console.log(genre);
+    console.log(rating);
+}
